85859b7bb96d33bcd8e7eb050788e9c7216e8109
Piyush <piyushgargofficial@gmail.com>
2021-11-25 23:47:25 -0500

    Blogger Website Completed

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+const responsive = {
+  0: {
+    items: 1,
+  },
+  320: {
+    items: 1,
+  },
+  560: {
+    items: 2,
+  },
+  960: {
+    items: 3,
+  },
+};
+
 $(document).ready(function () {
   $nav = $(".nav");
   $toggleCollapse = $(".toggle-collapse");
@@ -9,4 +24,25 @@ $(document).ready(function () {
 });
 
 //owl-carousel
-$(".owl-carousel").owlCarousel();
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  autoplay: false,
+  autoplayTimeout: 3000,
+  dots: false,
+  nav: true,
+  navText: [
+    $(".owl-navigation .owl-nav-prev"),
+    $(".owl-navigation .owl-nav-next"),
+  ],
+  responsive: responsive,
+});
+
+// click to scroll top
+$(".move-up span").click(function () {
+  $("html, body").animate(
+    {
+      scrollTop: 0,
+    },
+    1000
+  );
+});
